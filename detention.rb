81bf62108c97e63f14ea4322c366d8aa8547f7d1
twantkipp <twant@kippaustin.org>
2016-11-07 23:51:23 +0000

    hi

--- detention.rb
+++ detention.rb
@@ -1,1 +1,6 @@
-#Code your detention class here
+#Code your detention class here. Your class should:
+#1. Initialize with three attributes: location (i.e. "Rm 366"), time (i.e. "12:45 pm"), and an array of students (i.e. @students=[])
+#2. Be able to pass in the location and time as arguments upon initialization
+#3. Have a method called "add students" that takes a student name as an argument and adds it to the students list.
+#4. Have an attribute accessor for all three attributes
+#5. Bonus: Have additional methods or attributes of your choosing that help to better define detention. 
